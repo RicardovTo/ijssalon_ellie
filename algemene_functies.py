68c98d87aff9315737456bf1f0c151b39703e93d
Ricardo van Tongeren <cyra_819@hotmail.com>
2023-10-26 09:38:30 +0200

    functies verbeterd

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,13 +1,10 @@
 def mijn_functie_1(a):
-    print(a ** 2)
-
-
-
+    return a * a
 
 def mijn_functie_2(a, b):
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a / b)
-    return a, b
-
+    uitvoer_lijst =[]
+    uitvoer_lijst.append(a + b)
+    uitvoer_lijst.append(a - b)
+    uitvoer_lijst.append(a * b)
+    uitvoer_lijst.append(a / b)
+    return uitvoer_lijst
--- reclame.py
+++ reclame.py
@@ -1,52 +1,61 @@
 from algemene_functies import mijn_functie_2
 def aanbieding_1(smaak, prijs, korting):
-    totaal1 =  prijs * korting
-    totaal = prijs - totaal1
-    print(f"Vandaag in de aanbieding: 1 emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} voor {totaal} euro.")
+    prijs_na_korting = prijs * (1 - korting)
+    uitvoer = (f"Vandaag in de aanbieding: 1 emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} voor {prijs_na_korting} euro.")
+    return uitvoer
+print(aanbieding_1("aardbei", 4, 0.1))
 
-aanbieding_1("aardbei", 4, 0.1)
-print()
-print()
+def space():
+    print()
+    print()
+space()
+wekelijks = (220, 430, 125, 160, 205, 90, 345)
 
-wekelijks = [220, 430, 125, 160, 205, 90, 345]
+def inkomsten_totaal(inkomsten, btw):
+    totaal = 0
+    for bedrag in inkomsten:
+        totaal += bedrag
+        btw_bedrag = totaal * btw
+    uitvoer = (f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw_bedrag} euro betaald dient te worden.")
+    return uitvoer
+print(inkomsten_totaal(wekelijks, 0.09))
 
-def inkomsten_totaal(inkomsten):
+space()
 
-    totaal = (sum(inkomsten))
-    btw = totaal * 0.09
-    
-    print(f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw} euro btw betaald dient te worden.")
-inkomsten_totaal(wekelijks)
-print()
-print()
 def laag_en_hoog(mijn_lijst):
-    print(max(mijn_lijst))
-    print(min(mijn_lijst))
-    return mijn_lijst
-laag_en_hoog(wekelijks)
-print()
-print()
-def gemiddelde(mijn_lijst):
-    lengte = len(mijn_lijst)
-    som = sum(mijn_lijst)
-    gemiddelde = som / lengte
-    print(f"De gemiddelde inkomsten deze week zijn {gemiddelde} euro.")
-gemiddelde(wekelijks)
+    uitvoer = []
+    laagste = min(mijn_lijst)
+    hoogste = max(mijn_lijst)
+    uitvoer.append(laagste)
+    uitvoer.append(hoogste)
+    return uitvoer
 
-print()
-print()
-extra_lijst = [10, 5, 3, 2, 1, 2, 9]
+print(laag_en_hoog(wekelijks))
+
+space()
+
+def gemiddelde(mijn_lijst):
+    aantal = len(mijn_lijst)
+    totaal = 0
+    for element in mijn_lijst:
+        totaal += element
+        gemiddelde = totaal / aantal
+    return f"De gemiddelde inkomsten deze week zijn {gemiddelde} euro."
+print(gemiddelde(wekelijks))
+
+space()
+
+extra_lijst = 10, 5, 3, 2, 1, 2, 9
 
 def meervoudig(invoer_lijst):
-    laag_en_hoog(invoer_lijst)
-    return invoer_lijst
-meervoudig(extra_lijst)
-
-print()
-print()
+    tijdelijk = laag_en_hoog(invoer_lijst)
+    uitvoer = [tijdelijk[0],tijdelijk[1]]
+    return uitvoer
+print(meervoudig(extra_lijst))
+space()
 
 def combinatie(invoer_lijst_2):
     korte_lijst = laag_en_hoog(invoer_lijst_2)
     uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
     return uitvoer
-combinatie(extra_lijst)
+print(combinatie(extra_lijst))
